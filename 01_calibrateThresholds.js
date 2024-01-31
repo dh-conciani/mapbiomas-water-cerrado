72aa5337958a455ebfcace28ae4b034504ea9f58
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2024-01-31 09:28:51 -0300

    Update 01_calibrateThresholds.js

--- 01_calibrateThresholds.js
+++ 01_calibrateThresholds.js
@@ -30,7 +30,27 @@ var collection_s2 = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
 var collection_L8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
   .filterBounds(biomes_vec)
   .filter(ee.Filter.lt('CLOUD_COVER_LAND', cloudCover));
-                   
+  
+// set fixed parameters
+var pmtro_focal = {radius: 50, units: 'meters'};
+
+// sub-pixel endmembers
+var endmembers = [
+    [119.0, 475.0, 169.0, 6250.0, 2399.0, 675.0], /*gv*/
+    [1514.0, 1597.0, 1421.0, 3053.0, 7707.0, 1975.0], /*npv*/
+    [1799.0, 2479.0, 3158.0, 5437.0, 7707.0, 6646.0], /*soil*/
+    [4031.0, 8714.0, 7900.0, 8989.0, 7002.0, 6607.0] /*cloud*/
+];
+
+// sentinel-2 kernel 
+var kernelS2 = ee.Kernel.circle({radius: 2, units: 'pixels', magnitude: 1});
+var gaussianKernel = ee.Kernel.gaussian(3, 2, 'pixels', true, 2);
+
+// compute terrain models 
+var srtmBrasil = ee.Image('USGS/SRTMGL1_003').select("elevation");
+var terrain = ee.call('Terrain', srtmBrasil.convolve(gaussianKernel));
+
+
 //Map.addLayer(collection_S2)
 //print(collection_S2.size())
 //Map.addLayer(biomes.updateMask(biomes.eq(biome_id)).geometry())
